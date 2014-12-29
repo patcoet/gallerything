4071ff2bd06485624c037c91f24102d9a21fae96
username358 <saltsvan@gmail.com>
2014-12-29 13:06:10 +0100

    Message

--- gallery.php
+++ gallery.php
@@ -42,7 +42,7 @@ for ($i = 0; $i < count($imageFiles); $i++) {
 
   if (!$isInDB) {
     $db -> exec('INSERT INTO "'.$table.'" (name) VALUES ("'.$currFile.'")');
-    echo "adding $currFile...";
+    echo "adding $currFile...<br>\n";
   }
 }
 
