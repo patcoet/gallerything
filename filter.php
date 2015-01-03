745610467e5cf76b97deecb920813932d411c095
username358 <saltsvan@gmail.com>
2015-01-03 11:15:54 +0100

    not tag?

--- filter.php
+++ filter.php
@@ -40,6 +40,7 @@ if ($tag)
   $argument = 'SELECT name FROM "'.$table.'"';
 }
 $argument = $argument . ' ORDER BY rowid DESC';
+echo "$argument";
 $stmt = $db -> prepare($argument);
 $result = $stmt -> execute();
 $tempArray = array();
